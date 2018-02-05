be462e4c5dc4b42b3db8682035a3d25cb34080ea
Konstantinos Zisis <zisikons@gmail.com>
2018-02-05 17:37:20 +0200

    Added MIMO systems support and added documentation.
    Current version provides support for every operation (so far) needed in
    experiments and directly supports MIMO systems without needing to define
    a class for each subsystem.

--- utilities/ReferenceSystem/PolynomialReference.m
+++ utilities/ReferenceSystem/PolynomialReference.m
@@ -1,7 +1,37 @@
 classdef PolynomialReference < ReferenceSystem
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
-    
+%PolynomialReference Class providing utility functions for describing a
+%Polynomial reference system for MIMO systems.
+%
+%   A reference system is used to produce reference signals x_i(t) and  
+%   u_r(t), thus creating a trajectory for a real system to follow. The term
+%   polynomial refers to the assumption, that in this scenario the control
+%   input u(t) is a polynomial of t, resulting in every other state
+%   trajectory being also a polynomial of t. In order to use the polynomial
+%   reference system, one must specify a starting point x0, an ending point
+%   xT and the transition duration DT.
+%
+%   PolynomialReference(N,DT) instantiates a polynomial reference object of
+%   class order N, and a transitions duration time DT. N can be a positive
+%   integer, or a vector of positive integers, describing a MIMO system
+%   with relative degrees.
+%   
+%   Usage:
+%     % single input case
+%     pRef = PolynomialReference(2,0.2);
+%     x0 = [0;0];
+%     xT = [0;1];
+%
+%     % MIMO case (subsytems of order 2 and 1 accordingly)
+%     pRef = PolynomialReference([2,1],0.2);
+%     x0 = [0;0;0];
+%     xT = [0;1;2];
+%
+%     pRef.setTransition(x0, xT);  % parameterization for transition x0-xT
+%     pRef.controlInput(t)         % u_r(t) at given time t 
+%                                  % (see PolynomialReference/controInput) 
+%     pRef.plant(t,x_r)            % plant form for system integration
+%                                  % (see ReferenceSystem/plant)
+%
     properties
         parameters
         x0
@@ -9,38 +39,50 @@ classdef PolynomialReference < ReferenceSystem
         DT
         
         inputBoundary = 0;
-        inputCoeff         % polynomial coefficients for input calculation
+        inputCoeff           % polynomial coefficients for input calculation
         regressionMatrix
     end
     
-    methods  
+    methods
+        % Constructor
         function obj = PolynomialReference(N, DT)
             % Superclass constructor invocation
             obj = obj@ReferenceSystem(N);
             obj.DT = DT;     % transition time
-            
+
+            % Initiallize class internal variables
+            obj.parameters = cell(obj.M,1);
+            obj.inputCoeff = cell(obj.M,1);
+            obj.regressionMatrix = cell(obj.M,1);
+
             % Calculate internal parameters
             obj.updateStaticParameters();
-
-        end
-        
-        function u = controlInput(obj, t)
-            exponents = (obj.N+1):-1:0;
-            u = t.^exponents * (obj.inputCoeff .* obj.parameters(1:(obj.N+2)));
         end
         
         function updateStaticParameters(obj)
-        %UPDATESTATICPARAMETERS Utility function to calculate internal class
-        %parameters after N (order) and DT (transition time) selection.
-        %
-        % Depending only on the transition time DT and the system order N,
-        % every reference system of this type we can define a matrix R
-        % which is needed for calculating the model free parameters for a
-        % selected transition from a point x0 to a point xT. This function
-        % calculates these static attributes and assigns them to class
-        % variables for further calculations.
+        %updateStaticParameters Calls updateStaticParametersSI(N(i)) for every
+        %every subsystem.
+            for i = 1:obj.M
+                [obj.regressionMatrix{i}, ...
+                 obj.inputCoeff{i}]= obj.updateStaticParametersSI(obj.N(i));                  
+            end
+        end
         
-            N  = obj.N;
+        function [regressionMatrix, inputCoeff] = updateStaticParametersSI(obj,N)
+        %updateStaticParametersSI Utility function to calculate a single subsystem's  
+        %internal parameters.
+        %
+        % Depending only on the transition time DT and the sub-system order N,
+        % for every reference system of this type we can define a matrix R
+        % and a coefficents vector inputCoeff. Both of these attributes are
+        % needed for further calculations such as control input calculation
+        % and subsystem parameter vector calculation (solution of R*x = Y).
+        % This function is used in the constructor, and users normally should 
+        % never need to call it.
+        %
+        % Usage:
+        %   n = N(1);  % 1st substem order
+        %   obj.updateStaticParametersSI(n) % (after DT is specified)
             
             % Regressor Matix Calculation
             coefficients = zeros(N + 1, 2*(N + 1));
@@ -60,25 +102,79 @@ classdef PolynomialReference < ReferenceSystem
             end
             
             % Storage
-            obj.regressionMatrix = [coefficients .* 0.^exponents;...    % t start (assumed 0)
-                                    coefficients .* obj.DT.^exponents]; % t end (DT)
-            obj.inputCoeff = polyCoeff';
+            regressionMatrix = [coefficients .* 0.^exponents;...    % t start (assumed 0)
+                                coefficients .* obj.DT.^exponents]; % t end (DT)
+            inputCoeff = polyCoeff';
         end
         
-        function calculateParameters(obj, x0,xT)
+        function u = controlInput(obj, t)
+        %controlInput Calculate the control input for a parameterized
+        %system at time t, or for a time vector T.
+        %
+        % This function uses the internal variable parameters, so make sure
+        % it is called after a ref.setTransition(x0,xT) call.
+        %
+        % Usage:
+        %   %  Time instant t
+        %   u = pref.controlInput(t);
+        %   u = [u1(t)
+        %        u2(t)
+        %         ...
+        %        uM(t)]
+        %
+        %   % Time vector T
+        %   T = 0:0.1:DT;
+        %   u = pref.controlInput(T);
+        %   u = [ ----- u1(T) -----
+        %         ----- u1(T) -----
+        %                ...
+        %         ----- uM(T) -----]
+        %
+            % Initiallization
+            t = t(:);
+            u = zeros(obj.M, length(t));
+            
+            % Calculate input vector for each subsystem
+            for i = 1:obj.M
+                exponents = (obj.N(i)+1):-1:0;
+                T = bsxfun(@power,t,exponents); % vectorized form for offline calculations
+                modelParameters = obj.parameters{i};
+                
+                % ith-subsystem input vector
+                uV    = T * (obj.inputCoeff{i} .* modelParameters(1:(obj.N(i)+2)));
+                u(i,:) = uV';
+            end
+        end
+        
+        function setTransition(obj, x0, xT)
+        %setTransition Set parameters for the transition x0 -> xT
+        %
+        % Calculates and sets the system's free parameters in order to
+        % match the boundary conditions for a x0 to xT transition. 
+        % 
+        % Usage:
+        %   x0 = []; % column vector of system total size
+        %   xT = []; % column vector of system total size
+        %   pRef.setTransition(x0,xT);
+
             % Update internal variables
             obj.x0 = x0;
             obj.xT = xT;
             
-            % Transition parameters
-            Y = [x0(:); obj.inputBoundary; xT(:); obj.inputBoundary;];
-            obj.parameters = obj.regressionMatrix\Y;
-        end
-        
-        function offlineInput(obj, varargin)
+            % Auxilary matrix for partitioning input to subsystem states
+            subIndexes = [0;cumsum(obj.N)];
             
+            % Calculate model parameters for each subsystem
+            for i = 1:obj.M
+                Y = [x0(1+subIndexes(i) : subIndexes(i+1));
+                     obj.inputBoundary;
+                     xT(1+subIndexes(i) : subIndexes(i+1));
+                     obj.inputBoundary;];
+                
+                % Rx = Y system solution
+                obj.parameters{i} = obj.regressionMatrix{i}\Y;
+            end
         end
-        
     end
     
 end
--- utilities/ReferenceSystem/ReferenceSystem.m
+++ utilities/ReferenceSystem/ReferenceSystem.m
@@ -1,37 +1,72 @@
 classdef ReferenceSystem < handle
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
+%ReferenceSystem  Interface Class for MIMO reference systems.
+%
+%   A reference system is used to produce reference signals x_i(t) and  
+%   u_r(t), thus creating a trajectory for a real system to follow. This
+%   class provides a template for reference systems designed for MIMO
+%   systems in the canoncial form.
+%   
+%   Usage:
+%     % construct base object inside the derived class
+%     obj = obj@ReferenceSystem(N);
+%
+
     
     properties
-        N % class order
+        N     % class order matrix
+        M     % number of subsystems (maybe a more descriptive name needed)
     end
     
     methods
-        % Constructor
+
         function obj = ReferenceSystem(N)
-            obj.N = N;   % order
+            % Input verification
+            if (~isvector(N))
+               error('N must be a vector of positive integers.') 
+            end
+
+            % Store N array as a column vector
+            obj.M = length(N);
+            obj.N = N(:);
         end
         
-        % Plant Stracture Function
         function dx = plant(obj,t, x)
-            dx = ones(obj.N, 1);
-            
-            % System
+        %%Plant Differential equation form of MIMO system at (t,x)
+        %
+        %  This function is used to provide signals x_i(t) inside an ode
+        %  integration. The reference system is integrated alongside the 
+        %  actual sytem and plant(obj,t, x) provides the differential
+        %  equations of the reference system.
+        %
+        %  Usage:
+        %    % integrate the reference system alone
+        %    ref = RandomReference(N,varargin);
+        %    [T,X] = ode45(@ref.plant, t_sim, x0);
+        %
+        %    % integrate inside a controller
+        %    x_real = x(1:N);
+        %    x_ref   = x(N+1:2*N);
+        %
+        %    ...
+        %    dx(1:N)     = real_plant(t,x_real,u);
+        %    dx(N+1:2*N) = ref.plant(t,x_ref);
+
+            % Initiallization
+            dx = ones(sum(obj.N), 1);
+
+            % Bank of integrators
             dx(1:end-1) = x(2:end);
-            dx(end)     = obj.controlInput(t);   % or maybe control input of t?
+
+            % Assign control inputs
+            inputIndexes = cumsum(obj.N);
+            dx(inputIndexes) = obj.controlInput(t);
         end
+
     end
     
     methods (Abstract)
-        % Control Input for reference system
-        controlInput(obj, t)
-        
-        % Parameters for reference system
-        calculateParameters(obj, varargin)
-        
-        % Get the control input for the hole vector ? (X, T)
-        offlineInput(obj, varargin)
-        
+        % Control input vector for the reference system
+        controlInput(obj, varargin)
     end
     
 end
