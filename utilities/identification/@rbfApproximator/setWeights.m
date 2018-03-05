d765f7f8a6df6d47a4b6e1fd62a7b4e225dbf661
Konstantinos Zisis <zisikons@gmail.com>
2018-03-05 17:04:21 +0200

    Implemented setWeights method in rbfApproximator class.
    rbfApproximator.setWeight() reads an input vector, usually provided by
    the ode simulation, and correctly assigns the weights of the
    approximator. Notable:
     - Probably, there must be found a better alternative, since this method seems
       to be computantinally expensive. Avoid trasnpose and if possible reshaping.
     - Needs documentation.

---
+++ utilities/identification/@rbfApproximator/setWeights.m
@@ -0,0 +1,17 @@
+function [obj] =  setWeights(obj, w_hat)
+% Naive implementation. There is a tryhard alternative
+%
+%   current storage = [ --- wf1 weights ----, ---- wf2 weights ----, ...
+%                       --- wg11 weights ---, ----wg21 weights ----,...
+%                       --- wg21 weights ---, ----wg22 weights ----,...
+%                                            ... ]
+%   but I think this is very slow in closed loop.
+
+    % Defines who the weights are stored;
+    obj.wf = reshape(w_hat(1:(obj.M * obj.regressorSize_f)),...     % reshape
+                     obj.regressorSize_f, obj.M)';                  % the f part
+
+    obj.wg = reshape(w_hat(1+(obj.M * obj.regressorSize_f):end),... % reshape
+                     obj.regressorSize_g, obj.M^2)';                % the g part
+            
+end
