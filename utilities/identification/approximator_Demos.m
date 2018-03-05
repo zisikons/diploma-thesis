c8be950c67d20ca7cefade797eb573857e6f7008
Konstantinos Zisis <zisikons@gmail.com>
2018-03-05 02:34:34 +0200

    Script for testing the rbfApproximator class.
    (to be removed or updated in the upcoming commits)

---
+++ utilities/identification/approximator_Demos.m
@@ -0,0 +1,51 @@
+clear
+%% Case 1: Simple 2 order system, euler
+N = 2;
+centers = [0 0;
+           0 1;
+           1 0;
+           1 1]';
+
+f_parameters.centers  = centers;
+f_parameters.variance = 1;
+f_parameters.bias = true;
+
+g_parameters.centers = [0 1]; % exei ginei malakia
+                              % parolo poy den exo aktive states to pernaei
+g_parameters.variance = 1;
+g_parameters.active_states = 1;
+
+approx2D = rbfApproximator( N, f_parameters, 'g_parameters',g_parameters );
+disp(['Reduced g(x) 2D,size = ' ,num2str(approx2D.approximatorSize())])
+
+
+%% Case 2: Simple 2 order system, g(x) = full state
+approx2DFull = rbfApproximator( N, f_parameters);
+disp(['g(x) full state, size = ' ,num2str(approx2DFull.approximatorSize())])
+
+%% Case 3: MIMO [2,1], redused
+clear f_parameters g_parameters
+N = [2,1];
+centers = [0 0 0;
+           0 0 1;
+           0 1 0;
+           0 1 1;
+           1 0 0;
+           1 0 1;
+           1 1 0;
+           1 1 1;]';
+
+f_parameters.centers  = centers;
+f_parameters.variance = 1;
+f_parameters.bias = true;
+
+g_parameters.centers = [0 0;
+                        0 1;
+                        1 0;
+                        1 1]';
+g_parameters.variance = 1;
+g_parameters.active_states = [1,3];
+g_parameters.bias = true;
+
+approx3D = rbfApproximator( N, f_parameters, 'g_parameters',g_parameters );
+disp(['Reduced g(x) 3D, size = ' ,num2str(approx3D.approximatorSize())])
